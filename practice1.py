023433616a67b182d2f1336a6b6abf418a727765
keksoviy <90835218+pavliggg@users.noreply.github.com>
2024-07-09 21:08:47 +0300

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,12 @@
+print('"1st program"')
+print(9**0.5*5)
+print('"2nd program"')
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('"3rd program"')
+num1 = 1234
+num2 = 5678
+print(num1 % 1000 // 10 + num2 % 1000 // 10)
+print('"4th program"')
+num3 = 13.42
+num4 = 42.13
+print((num3 * 100 % 100) == (int(num4))) or ((num4 * 100 % 100) == (int(num3)))
