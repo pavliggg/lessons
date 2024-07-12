e13e93d8e01e5e74e66d8b58d53b6f6e4855d418
keksoviy <90835218+pavliggg@users.noreply.github.com>
2024-07-12 19:44:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_ht = 12
+Number_of_hours = 1.5
+Course = "Python"
+Time_to_complete = (Number_of_hours / Number_of_ht )
+print(Course, ', всего задач:', Number_of_ht, ', затрачено часов:', Number_of_hours, ', среднее время выполнения', Time_to_complete)
