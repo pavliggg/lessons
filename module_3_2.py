d5f4d8e994d71794cef9e958bcffd0d01bb90945
keksoviy <90835218+pavliggg@users.noreply.github.com>
2024-08-03 14:21:07 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,18 +1,15 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     def is_valid_email(email):
         return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
-
     if not is_valid_email(sender):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
     if not is_valid_email(recipient):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
-
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
-
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
