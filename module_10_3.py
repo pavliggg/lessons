859ca665dd0dfa451746e65c9970c43072da2949
keksoviy <90835218+pavliggg@users.noreply.github.com>
2024-12-14 18:10:10 +0300

    module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -12,22 +12,22 @@ class Bank:
             amount = random.randint(50, 500)
             with self.lock:
                 self.balance += amount
-                if self.balance >= 500 and self.lock.locked():
+                print(f'Пополнение: {amount}. Баланс: {self.balance}')
+                if self.balance >= 500:
                     self.lock.release()
-                print(f"Пополнение: {amount}. Баланс: {self.balance}")
-            time.sleep(0.001)
+            time.sleep(0.01)
 
     def take(self):
         for _ in range(100):
             amount = random.randint(50, 500)
-            print(f"Запрос на {amount}")
+            print(f'Запрос на {amount}')
             with self.lock:
                 if amount <= self.balance:
                     self.balance -= amount
-                    print(f"Снятие: {amount}. Баланс: {self.balance}")
+                    print(f'Снятие: {amount}. Баланс: {self.balance}')
                 else:
-                    print("Запрос отклонён, недостаточно средств")
-                    self.lock.acquire()
+                    print('Запрос отклонён, недостаточно средств')
+            time.sleep(0.001)
 
 bk = Bank()
 
